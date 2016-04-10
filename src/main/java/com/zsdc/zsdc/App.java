16cef7ba209f0661b4fda5f2b5af7cc0a75bb0aa
qys0730 <qys0730@163.com>
2016-04-10 12:06:55 +0800

    Signed-off-by: qys0730 <qys0730@163.com>

---
+++ src/main/java/com/zsdc/zsdc/App.java
@@ -0,0 +1,13 @@
+package com.zsdc.zsdc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
